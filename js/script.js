97dedf8ae3db47a480de591ef4a1d77472ea66bf
Rawanbahaa142 <rwanalrawy162@gmail.com>
2025-08-20 23:12:48 +0300

    Add Scroll Button

--- js/script.js
+++ js/script.js
@@ -1,27 +1,20 @@
-const filterLinks = document.querySelectorAll(".options a");
-const cards = document.querySelectorAll(".cards .card");
+      // Scroll to Top Button
+window.addEventListener("scroll", () => {
+   
+  let scrollDiv = document.getElementById("scrollDiv");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollDiv.style.display = "block";
+  } else {
+    scrollDiv.style.display = "none";
+  }
+});
 
-filterLinks.forEach(link => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault(); 
-    filterLinks.forEach(l => l.classList.remove("active"));
-      link.classList.add("active");
-      
-    const filter = link.getAttribute("data-filter");
-
-    cards.forEach(card => {
-      if (filter === "all") {
-        card.style.display = "block";
-      } else {
-        const categories = card.getAttribute("data-category").split(" ");
-        if (categories.includes(filter)) {
-          card.style.display = "block";
-        } else {
-          card.style.display = "none";
-        }
-      }
-    });
+document.getElementById("scrollDiv").addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
   });
 });
+////////////////////////////////////////
 
   
